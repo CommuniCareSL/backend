8e65d98c243547d746e904eb9250a6b15c7da1c5
AshenSachinthana <dasachinthana@gmail.com>
2025-01-14 17:00:57 +0530

    feat(holiday): add methods to create holiday and retrieve holiday dates

--- src/appointment/holiday/holiday.controller.ts
+++ src/appointment/holiday/holiday.controller.ts
@@ -1,8 +1,18 @@
 import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { HolidayService } from './holiday.service';
+import { Holiday } from './holiday.model';
 
 @Controller('holiday')
 export class HolidayController {
     constructor(private readonly holidayService: HolidayService) {}
 
+    @Post()
+    async addHoliday(@Body() holiday: Holiday): Promise<Holiday> {
+        return this.holidayService.addHoliday(holiday);
+    }
+
+    @Get()
+    async getHolidayDates(): Promise<string[]> {
+        return this.holidayService.getHolidayDates();
+    }
 }
--- src/appointment/holiday/holiday.service.ts
+++ src/appointment/holiday/holiday.service.ts
@@ -8,4 +8,17 @@ export class HolidayService {
         @InjectModel(Holiday)
         private holidayModel: typeof Holiday
     ) {}
+
+    async addHoliday(holiday: Holiday): Promise<Holiday> {
+        return this.holidayModel.create(holiday);
+    }
+
+    async getHolidayDates(): Promise<string[]> {
+        const holidays = await this.holidayModel.findAll({
+            attributes: ['holidayDate'], // Only fetch the `holidayDate` field
+        });
+
+        // Explicitly cast `holidayDate` to a Date object
+        return holidays.map(holiday => new Date(holiday.holidayDate).toISOString().split('T')[0]);
+    }
 }
